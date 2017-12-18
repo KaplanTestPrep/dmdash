de90f916b0d3abdc039f462e985595b60f7b5a7e
irman33 <irman.ahmetovic@kaplan.com>
2017-12-18 15:38:19 -0500

    prettier save

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,24 +11,19 @@ exports.logout = (req, res) => {
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     console.log("PATH:", req.route.path);
-    if(req.route.path === "/login"){
+    if (req.route.path === "/login") {
       console.log("/login route and authenticated");
       res.redirect("/");
       return false;
     }
-    
+
     next();
     return;
-  }
-<<<<<<< HEAD
-  req.flash("warning", "Oops, you must be logged in to do that!");
-=======
-  else if(req.route.path === "/login"){
+  } else if (req.route.path === "/login") {
     next();
     return false;
   }
   req.flash("danger", "Oops, you must be logged in to do that!");
->>>>>>> 56a66973424ac4f2c14b076012a05dc8be29ae27
   res.redirect("/login");
 };
 
@@ -41,18 +36,9 @@ exports.isLoggedIn = (req, res, next) => {
 //   });
 // };
 
-<<<<<<< HEAD
-// exports.googleAuthCallback = () => {
-//   passport.authenticate("google", { failureRedirect: "/login" }),
-//     function(req, res) {
-//       res.redirect("/dashboard");
-//     };
-// };
-=======
 exports.authCallback = () => {
   passport.authenticate("google", { failureRedirect: "/login" }),
     function(req, res) {
       res.redirect("/");
     };
 };
->>>>>>> 56a66973424ac4f2c14b076012a05dc8be29ae27
--- controllers/zoomController.js
+++ controllers/zoomController.js
@@ -1,118 +1,107 @@
-const jwt = require('jsonwebtoken');
-const axios = require('axios');
-const { moment } = require('../helpers');
-
-
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const { moment } = require("../helpers");
 
 exports.recordings = (req, res) => {
-    res.render("zoomRecordings", { pageTitle: "Zoom Recordings", active: "zoom" });
-}
+  res.render("zoomRecordings", {
+    pageTitle: "Zoom Recordings",
+    active: "zoom"
+  });
+};
 
 exports.recordingsData = async (req, res) => {
+  // Get list of all users
+  const url = "https://api.zoom.us/v2/users";
+  const token = exports.getToken();
 
-    // Get list of all users
-    const url = 'https://api.zoom.us/v2/users';
-    const token = exports.getToken();
-    
-    const response = await axios.get(url, {
+  const response = await axios.get(url, {
+    params: {
+      access_token: token
+    }
+  });
+
+  const users = response.data.users;
+  console.log(users);
+
+  // Get list of recordings for each user
+  const endDate = moment()
+    .toISOString()
+    .split("T")[0];
+  const startDate = moment()
+    .subtract(6, "months")
+    .toISOString()
+    .split("T")[0];
+  let meetings = [];
+  let recordings = [];
+
+  // console.log({startDate, endDate});
+
+  await Promise.all(
+    users.map(async user => {
+      const url = `https://api.zoom.us/v2/users/${user.id}/recordings`;
+      const token = exports.getToken();
+      let recording = {};
+
+      const response = await axios.get(url, {
+        params: {
+          access_token: token,
+          userId: user.id,
+          from: startDate,
+          to: endDate
+        }
+      });
+
+      const responsMeetings = response.data.meetings;
+
+      // if (typeof responsMeetings !== 'undefined' && responsMeetings.length > 0) {
+    })
+  );
+
+  let justMeetings = [];
+  meetings.forEach(meeting => {
+    justMeetings = [...justMeetings, ...meeting.recording_files];
+  });
+
+  const data = { data: meetings };
+
+  //   console.log(justMeetings);
+
+  res.send(data);
+};
+
+exports.users = (req, res) => {
+  res.render("zoomUsers", { pageTitle: "Zoom Users", active: "zoom" });
+};
+
+exports.getToken = () => {
+  const payload = {
+    iss: process.env.ZOOMAPIKEY,
+    exp: new Date().getTime() + 5000
+  };
+  //Automatically creates header, and returns JWT
+  var token = jwt.sign(payload, process.env.ZOOMAPISECRET);
+
+  return token;
+};
+
+exports.getDailyReport = () => {
+  const url = "https://api.zoom.us/v2/report/daily";
+  const token = exports.getToken();
+
+  return new Promise(resolve => {
+    axios
+      .get(url, {
         params: {
           access_token: token
         }
-    });
-
-    const users = response.data.users;
-    console.log(users);
-    
-    // Get list of recordings for each user
-    const endDate = moment().toISOString().split('T')[0];
-    const startDate = moment().subtract(6, 'months').toISOString().split('T')[0];
-    let meetings = [];
-    let recordings = [];
-    
-    // console.log({startDate, endDate});
-
-    await Promise.all(users.map(async (user) => {
-        const url = `https://api.zoom.us/v2/users/${user.id}/recordings`;
-        const token = exports.getToken();
-        let recording = {};
-
-        const response = await axios.get(url, {
-            params: {
-              access_token: token,
-              userId: user.id,
-              from: startDate,
-              to: endDate
-            }
-        });
-        
-        const responsMeetings = response.data.meetings;
-    
-        // if (typeof responsMeetings !== 'undefined' && responsMeetings.length > 0) {
-            
-        //    console.log(responsMeetings, "---------------------/n")
-        const recordings = (({ a, c }) => ({ a, c }))(object);
-        responsMeetings.forEach( (meeting) => {
-            recording = "hELLO";
-    
-            
-         });
-            
-             
-        }
-
-        meetings = [...meetings, ...response.data.meetings];
-      }));
-
-      let justMeetings = [];
-      meetings.forEach(meeting => {
-        justMeetings = [...justMeetings, ...meeting.recording_files];
       })
-
-      const data = { data: meetings};
-
-    //   console.log(justMeetings);
-
-    res.send(data);
-    
-}
-
-
-
-
-exports.users = (req, res) => {
-    res.render("zoomUsers", { pageTitle: "Zoom Users", active: "zoom" });
-}
-
-
-exports.getToken = () => {
-    const payload = {
-        iss: process.env.ZOOMAPIKEY,
-        exp: ((new Date()).getTime() + 5000)
-    };
-    //Automatically creates header, and returns JWT
-    var token = jwt.sign(payload, process.env.ZOOMAPISECRET);
-        
-    return token;
-}
-
-exports.getDailyReport = () => {
-    const url = 'https://api.zoom.us/v2/report/daily';
-    const token = exports.getToken();
-    
-    return new Promise( resolve => {
-        axios.get(url, {
-            params: {
-              access_token: token
-            }
-        })
-        .then(function (response) {
-            // console.log("Response:", response.data);
-            const todaysDate = new Date().toISOString().split('T')[0];
-            const dailyReportObj = response.data.dates.filter(report => report.date === todaysDate);
-            resolve(dailyReportObj[0]);
-        });
-    })
-}
-
-
-
+      .then(function(response) {
+        // console.log("Response:", response.data);
+        const todaysDate = new Date().toISOString().split("T")[0];
+        const dailyReportObj = response.data.dates.filter(
+          report => report.date === todaysDate
+        );
+        resolve(dailyReportObj[0]);
+      });
+  });
+};
