e6931f2a826ee81bbf4d81d545967190202354e9
irman33 <irman.ahmetovic@kaplan.com>
2018-01-01 16:23:43 -0500

    BC config file

---
+++ config/bcConfig.js
@@ -0,0 +1,57 @@
+exports.bcCredentials = {
+  serviceUrl: "https://oauth.brightcove.com/v3/access_token",
+  clientId: "49c143b5-5de8-455c-9d2c-a2f45c487d2e",
+  client_secret: "7AZvDiX1WpUHX2pawI50uNEJDiqV02ftCilaFDzzJayitgfsSTDFiU815lq0n5YN31Td93MwfL90AS-AEemN8Q"
+}
+
+exports.brightcoveAccounts = [
+  {
+    name: "ATOM - Kaplan Test Prep",
+    id: 1107601866001
+  },
+  {
+    name: "DM Production and Studio (Internal) - Kaplan Test Prep",
+    id: 958462685001
+  },
+  {
+    name: "Digital Media Sandbox - Kaplan Test Prep",
+    id: 5431089189001
+  },
+  {
+    name: "Pseudo Synch Pod - Kaplan Test Prep",
+    id: 3399141178001
+  },
+  {
+    name: "Kaptest (Marketing) - Kaplan Test Prep",
+    id: 1107601868001
+  },
+  {
+    name: "ACT Benchprep / Gallery Delivery - Kaplan Test Prep",
+    id: 5069692952001
+  },
+  {
+    name: "GED Gallery Delivery - Kaplan Test Prep",
+    id: 5463980167001
+  },
+  {
+    name: "BC Mobile App - Kaplan Test Prep",
+    id: 5410596369001
+  },
+  {
+    name: "Advisestream - Kaplan Test Prep",
+    id: 5463980168001
+  },
+  {
+    name: "Health (Legacy) - Kaplan Test Prep",
+    id: 1107601867001
+  },
+  {
+    name: "PreCollege (Legacy) - Kaplan Test Prep",
+    id: 1107601869001
+  },
+  {
+    name: "KTPi (Legacy) - Kaplan Test Prep",
+    id: 2546882126001
+  }
+]
+
