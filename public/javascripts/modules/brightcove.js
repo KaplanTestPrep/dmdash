a9f9329425fb9c6a74c26d5e454b91bbe870485f
irman33 <irman.ahmetovic@kaplan.com>
2018-03-23 17:11:19 -0400

    consolidated single/batch functions into one, streamlined elem namings

--- public/javascripts/modules/brightcove.js
+++ public/javascripts/modules/brightcove.js
@@ -6,11 +6,10 @@ $(document).ready(function () {
 
     $('#videoRenditions').click((e) => handleVideoRenditions(e));
     $('#batchRetranscode').click((e) => handleBatchRetranscode(e));
-    $("#imageUploadForm").submit((e) => handleImageUploadForm(e));
+    $("#thumbnailUploadForm").submit((e) => handleThumbnailUploadForm(e));
     $('#thumbnailUpdateForm').submit((e) => handleThumbnailUpdateForm(e));
     $('#refIdUpdateForm').submit((e) => handleRefIdUpdateForm(e));
-    $('#mediaShareSingleForm').submit((e) => handleMediaShareSingleForm(e));
-    $('#mediaShareBatchForm').submit((e) => handleMediaShareBatchForm(e));
+    $('#mediaShareForm').submit((e) => handleMediaShareForm(e));
     $('#metadataUpdateForm').submit((e) => handleMetadataCSV(e));
     $('#removeTextTrackForm').submit((e) => handleRemoveTextTrack(e));
 
@@ -164,7 +163,7 @@ function handleBatchRetranscode(e) {
     });
 }
 
-function handleImageUploadForm(e) {
+function handleThumbnailUploadForm(e) {
     $("span#status").removeClass();
     $("span#status").text("");
     $("span#status").addClass('text-warning');
@@ -197,7 +196,7 @@ function handleThumbnailUpdateForm(e) {
     e.preventDefault();
     const accountId = $('#bcAcccount').val();
     const videos = $('#vidoesToUpdate').val().trim().split(/[\r\n\s,]+/);
-    const idType = $('input[name=idType]:checked').val();
+    const refType = $('input[name=refType]:checked').val();
     const thumbnailFileName = $('#uploadedImage').val();
     let completed = 0
     let fail = 0;
@@ -213,8 +212,8 @@ function handleThumbnailUpdateForm(e) {
         type: "POST",
         data: {
           accountId,
-          videoId: video,
-          idType,
+          ref: video,
+          refType,
           thumbnailFileName
         }
       })
@@ -238,9 +237,8 @@ function handleThumbnailUpdateForm(e) {
 }
 
 
-function handleRefIdUpdateForm(e) {
+async function handleRefIdUpdateForm(e) {
     e.preventDefault();
-    
     $('#resultsCard').removeClass('hidden');
     $('ul#success').html(""); 
     $('ul#fail').html(""); 
@@ -250,133 +248,82 @@ function handleRefIdUpdateForm(e) {
     let total = 1
     
     const accountId = $('#bcAccount').val();
-    const oldId = $('#oldId').val().trim();
-    const idType = $('input[name=idType]:checked').val();
-    const newRefId = $('#newRefId').val().trim();
+    const refType = $('input[name=refType]:checked').val();
+    const file = document.getElementById('selectCSV').files[0];
+    if (!file) return;
 
-    $.ajax({
-      url: `/metadataUpdateTool`,    //refIdUpdateTool 
-      type: "POST",
-      data: {
-        accountId,
-        idType,
-        ref: oldId,
-        reference_id: newRefId
-      }
-    })
-    .done(res => {
-      console.log(res);
-      completed++;
-      $('.progress-bar').css("width", `${(completed/total)*100}%`);
-      $("#percentage").text(`Progress: ${Math.round((completed/total)*100)}%`);
-      $('ul#success').append(`<li>${oldId} --> ${newRefId} Sucessfully processsed.</li>`);
-    })
-    .fail(err => {
-      completed++;
-      fail++;
+    let results = await papaPromisified(file);
+    let videos = results.data
+    videos.shift();
+
+    console.log(videos);
+
+    total = videos.length;
+
+    videos.forEach(video => {
+      let [ref, newRefId] = [...video];
       
-      console.log(`${oldId} --> ${newRefId} Failed: ${err.responseText}`, err);
-      $('.progress-bar').css("width", `${(completed/total)*100}%`);
-      $("#percentage").text(`Progress: ${Math.round((completed/total)*100)}%`);
-      $('ul#fail').append(`<li>${oldId} > ${newRefId} Failed: ${err.responseText}</li>`);
+      $('#resultsCard').removeClass('hidden');
+      $('ul#success').html(""); 
+      $('ul#fail').html(""); 
+  
+      $.ajax({
+        url: `/refIdUpdate`,
+        type: "POST",
+        data: {
+          accountId,
+          ref,
+          refType,
+          reference_id: newRefId
+        }
+      })
+      .done(res => {
+        console.log(res);
+        completed++;
+        $('.progress-bar').css("width", `${(completed/total)*100}%`);
+        $("#percentage").text(`Progress: ${Math.round((completed/total)*100)}%`);
+        $('ul#success').append(`<li>${ref} --> Sucessfully processsed.</li>`);
+      })
+      .fail(err => {
+        completed++;
+        fail++;
+        
+        console.log(`${ref} --> Failed: ${err.responseText}`, err);
+        $('.progress-bar').css("width", `${(completed/total)*100}%`);
+        $("#percentage").text(`Progress: ${Math.round((completed/total)*100)}%`);
+        $('ul#fail').append(`<li>${ref} > Failed: ${err.responseText}</li>`);
+      });
     })
+  
+    document.getElementById('refIdUpdateForm').reset();
 }
 
-
-
-
-
-
-
-
-async function handleRefIdUpdateBatchForm(e) {
-  $("span#status").removeClass();
-  $("span#status").text("");
+function handleMediaShareForm(e) {
   e.preventDefault();
+  $('#resultsCard').removeClass('hidden');
+  $('ul#success').html(""); 
+  $('ul#fail').html(""); 
 
-  let percentDone = 0;
   let completed = 0
   let fail = 0;
-  let total = 0;
+  let total = 1
+  
+  const accountIdSource = $('#bcAccountSource').val();
+  const accountIdDest = $('#bcAccountDest').val();
+  const videos = $('#vidoesToUpdate').val().trim().split(/[\r\n\s,]+/);
+  const refType = $('input[name=refType]:checked').val();
+  let ref = "";
 
-  const accountId = $('#bcAccountBatch').val();
-  const idType = $('input[name=idTypeBatch]:checked').val();
-  let file = document.getElementById('selectCSV').files[0];
-  if (!file) return;
-
-  let results = await papaPromisified(file);
-  console.log("For real:", results);
-  total = results.length;
-
-  let data = results.data
-  data.shift();
-
-  data.forEach(video => {
-    let [refId, title, tags, shortDescription] = [...video];
-    let tagsArr = tags.split(",");
-    console.log(refId, title, tags, shortDescription, accountId, idType);
-
-    $('#resultsCard').removeClass('hidden');
-    $('ul#success').html(""); 
-    $('ul#fail').html(""); 
-
-    $.ajax({
-      url: `/metadataUpdateTool`,
-      type: "POST",
-      data: {
-        accountId,
-        idType,
-        ref: refId,
-        reference_id: refId,
-        name: title,
-        tags: tagsArr,
-        description: shortDescription
-      }
-    })
-    .done(res => {
-      console.log(res);
-      completed++;
-      $('.progress-bar').css("width", `${(completed/total)*100}%`);
-      $("#percentage").text(`Progress: ${Math.round((completed/total)*100)}%`);
-      $('ul#success').append(`<li>${refId} --> Sucessfully processsed.</li>`);
-    })
-    .fail(err => {
-      completed++;
-      fail++;
-      
-      console.log(`${refId} --> Failed: ${err.responseText}`, err);
-      $('.progress-bar').css("width", `${(completed/total)*100}%`);
-      $("#percentage").text(`Progress: ${Math.round((completed/total)*100)}%`);
-      $('ul#fail').append(`<li>${refId} > Failed: ${err.responseText}</li>`);
-    });
-  })
-
-  document.getElementById('refIdUpdateBatchForm').reset();
-}
-
-function handleMediaShareSingleForm(e) {
-    e.preventDefault();
-    $('#resultsCard').removeClass('hidden');
-    $('ul#success').html(""); 
-    $('ul#fail').html(""); 
-
-    let completed = 0
-    let fail = 0;
-    let total = 1
-    
-    const accountIdSource = $('#bcAccountSource').val();
-    const accountIdDest = $('#bcAccountDest').val();
-    const refId = $('#refId').val().trim();
-    const idType = $('input[name=idType]:checked').val();
-
+  videos.forEach(video => {
+    ref = video;
     $.ajax({
       url: `/mediaShare`,
       type: "POST",
       data: {
         accountIdSource,
         accountIdDest,
-        refId,
-        idType
+        ref,
+        refType
       }
     })
     .done(res => {
@@ -384,94 +331,20 @@ function handleMediaShareSingleForm(e) {
       completed++;
       $('.progress-bar').css("width", `${(completed/total)*100}%`);
       $("#percentage").text(`Progress: ${Math.round((completed/total)*100)}%`);
-      $('ul#success').append(`<li>Moved ${refId} from ${accountIdSource} to ${accountIdSource} - Sucessfully processsed.</li>`);
+      $('ul#success').append(`<li>Moved ${ref} from ${accountIdSource} to ${accountIdSource} - Sucessfully processsed.</li>`);
     })
     .fail(err => {
       completed++;
       fail++;
       
-      console.log(`Moving ${refId} from ${accountIdSource} to ${accountIdSource} - Failed: ${err.responseText}`, err);
+      console.log(`Moving ${ref} from ${accountIdSource} to ${accountIdSource} - Failed: ${err.responseText}`, err);
       $('.progress-bar').css("width", `${(completed/total)*100}%`);
       $("#percentage").text(`Progress: ${Math.round((completed/total)*100)}%`);
-      $('ul#fail').append(`<li>Moving ${refId} from ${accountIdSource} to ${accountIdDest} - Failed: ${err.responseText}</li>`);
-
-    })
-}
-
-function handleMediaShareBatchForm(e) {
-    $("span#status").removeClass();
-    $("span#status").text("");
-    $("span#status").addClass('text-warning');
-    $("span#status").text("  Uploading...");
-    e.preventDefault();
-
-    const accountIdSource = $('#bcAccountSourceBatch').val();
-    const accountIdDest = $('#bcAccountDestBatch').val();
-    const idType = $('input[name=idTypeBatch]:checked').val();
-
-    const fileSelect = document.getElementById('selectCSVBatch');
-    console.log(fileSelect);
-
-    const files = fileSelect.files;
-    let form = new FormData();
-    form.append('csv', files[0], files[0].name);
-    
-    $.ajax({
-      url: '/mediaShareBatch',
-      data: form,
-      processData: false,
-      contentType: false,
-      type: 'POST',
-      success: function(data){
-        const dataArr = JSON.parse(data);
-
-        $("span#status").removeClass();
-        $("span#status").addClass('text-success');
-        $("span#status").text("  Upload Completed!");
-
-        var completed = 0
-        var fail = 0;
-        var total = dataArr.length;
-
-
-        console.log(dataArr);
-        dataArr.forEach(item => {
-          let refId = item;
-     
-          $('#resultsCard').removeClass('hidden');
-          $('ul#success').html(""); 
-          $('ul#fail').html(""); 
-
-          $.ajax({
-            url: `/mediaShare`,
-            type: "POST",
-            data: {
-              accountIdSource,
-              accountIdDest,
-              idType,
-              refId: item
-            }
-          })
-          .done(res => {
-            completed++;
-            $('.progress-bar').css("width", `${(completed/total)*100}%`);
-            $("#percentage").text(`Progress: ${Math.round((completed/total)*100)}%`);
-            $('ul#success').append(`<li>Moved ${refId} from ${accountIdSource} to ${accountIdDest} - Sucessfully processsed.</li>`);
-          })
-          .fail(err => {
-            completed++;
-            fail++;
-            
-            console.log(`Moving ${refId} from ${accountIdSource} to ${accountIdDest} - Failed: ${err.responseText}`, err);
-            $('.progress-bar').css("width", `${(completed/total)*100}%`);
-            $("#percentage").text(`Progress: ${Math.round((completed/total)*100)}%`);
-            $('ul#fail').append(`<li>Moving ${refId} from ${accountIdSource} to ${accountIdDest} - Failed: ${err.responseText}</li>`);
-          })
-        });
-      }
+      $('ul#fail').append(`<li>Moving ${ref} from ${accountIdSource} to ${accountIdDest} - Failed: ${err.responseText}</li>`);
+  
     });
+  });
 }
-
 
 async function handleMetadataCSV(e) {
   e.preventDefault();
@@ -480,19 +353,18 @@ async function handleMetadataCSV(e) {
   let fail = 0;
   let total = 0;
 
-  const accountId = $('#bcAccountBatch').val();
-  const refType = $('input[name=idTypeBatch]:checked').val();
-
-  let file = document.getElementById('selectCSV').files[0];
+  const accountId = $('#bcAccount').val();
+  const refType = $('input[name=refType]:checked').val();
+  const file = document.getElementById('selectCSV').files[0];
   if (!file) return;
 
   let results = await papaPromisified(file);
-  let data = results.data
-  data.shift();
+  let videos = results.data
+  videos.shift();
 
-  total = data.length;
+  total = videos.length;
 
-  data.forEach(video => {
+  videos.forEach(video => {
     let [ref, name, tags, description] = [...video];
     let tagsArr = tags.split(",");
     console.log(ref, name, tags, description, accountId, refType);
