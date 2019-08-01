ad373827386383cec76f6b73c8b483e4d06d3d17
Irman Ahmetovic <IAhmetovic@kaplaninc.com>
2019-08-01 16:32:54 -0400

    removed comments

--- controllers/dashController.js
+++ controllers/dashController.js
@@ -3,7 +3,9 @@ const wowza = require("./wowzaController");
 
 exports.getDashboard = async (req, res) => {
   const dailyZoomReport = await zoom.getDailyReport();
+  // not being used
   // const wowzaServerStatus = await wowza.getStatus();
+  //
 
   res.render("dashboard", {
     title: "Dashboard",
